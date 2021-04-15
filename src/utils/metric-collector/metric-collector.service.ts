f86f9282345b97fe5fe37c3659452a997079da3a
Alexander Eberle <alexander171294@gmail.com>
2021-04-15 18:56:19 -0300

    clean metric collector

--- src/utils/metric-collector/metric-collector.service.ts
+++ src/utils/metric-collector/metric-collector.service.ts
@@ -1,51 +1,18 @@
 import { environments } from 'src/environment';
 import { Injectable, Logger } from '@nestjs/common';
-import * as elasticsearch from 'elasticsearch';
 import * as InfluxDB from 'influxdb-nodejs';
-
-const AppMetrics = require('appmetrics');
 
 @Injectable()
 export class MetricCollectorService {
 
     private readonly logger = new Logger(MetricCollectorService.name);
 
-    private readonly esclient: elasticsearch.Client;
     private readonly influx;
     private schemas: {[key: string]: boolean} = {};
     private readonly metricID = environments.appMetricID;
 
     constructor() {
         this.influx = new InfluxDB(environments.influxDB);
-        this.globalMetrics(AppMetrics.monitor());
-    }
-
-    globalMetrics(monitoring) {
-        this.setMetricSchema('requirements', {
-            type: ['cpu', 'memory']
-        },{
-            used: SchemaDataType.FLOAT,
-            total_used: SchemaDataType.FLOAT,
-            free: SchemaDataType.FLOAT
-        })
-        monitoring.on('cpu', (cpu) => {
-            const used = cpu.process;
-            const total_used = cpu.system;
-            this.writeMetric('requirements', {type: 'cpu'}, {
-                used,
-                total_used
-            });
-        });
-        monitoring.on('memory', (cpu) => {
-            const used = cpu.physical;
-            const total_used = cpu.physical_used;
-            const free = cpu.physical_free;
-            this.writeMetric('requirements', {type: 'cpu'}, {
-                used,
-                total_used,
-                free
-            });
-        });
     }
 
     public setMetricSchema(metricName: string, tags: {[key: string]: string[] | '*'}, fields: {[key:string]: SchemaDataType}) {
