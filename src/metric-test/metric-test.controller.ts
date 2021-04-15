16b234c19674047b7f2a9241cb8da86e39c8d312
Alexander Eberle <alexander171294@gmail.com>
2021-04-15 15:19:12 -0300

    adding field

--- src/metric-test/metric-test.controller.ts
+++ src/metric-test/metric-test.controller.ts
@@ -1,13 +1,15 @@
-import { MetricCollectorService } from 'src/utils/metric-collector/metric-collector.service';
+import { MetricCollectorService, SchemaDataType } from 'src/utils/metric-collector/metric-collector.service';
 import { Body, Controller, Get, Post } from '@nestjs/common';
 
 @Controller('metric-test')
 export class MetricTestController {
 
     constructor(private metricCollector: MetricCollectorService) {
-        this.metricCollector.setMetricSchema('metric-test', {
+        this.metricCollector.setMetricSchema('metric-test', { // tags:
             status: ['ok', 'nok']
-        },{});
+        },{ // Data of this request:
+            response: SchemaDataType.STRING
+        });
     }
 
     @Post()
@@ -20,6 +22,8 @@ export class MetricTestController {
     writeMetricAlt() {
         this.metricCollector._writeMetric('metric-test', {
             status: Math.random() > 0.5 ? 'ok' : 'nok'
+        }, {
+            response: 'DONE'
         });
         return '"DONE"';
     }
