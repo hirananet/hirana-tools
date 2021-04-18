73a7007e064c31c1fd1e52e66b8b9a24f5739bc6
Alexander Eberle <alexander171294@gmail.com>
2021-04-18 16:58:44 -0300

    imgur metric collector

--- src/imgur-api/imgur-api.controller.ts
+++ src/imgur-api/imgur-api.controller.ts
@@ -1,22 +1,32 @@
 import { ImgurService } from './imgur.service';
 import { Body, Controller, Post, Res } from '@nestjs/common';
 import { Response } from 'express';
+import { MetricCollectorService } from 'src/utils/core-utils/metric-collector/metric-collector.service';
+import { RealIP } from 'nestjs-real-ip';
 
 @Controller('upload')
 export class ImgurApiController {
 
-    constructor(private imgurSrv: ImgurService) {
-    }
+    constructor(private imgurSrv: ImgurService, private metricCollector: MetricCollectorService) { }
 
     @Post()
-    public name(@Res() response: Response, @Body() body: ImageInput) {
+    public name(@Res() response: Response, @Body() body: ImageInput, @RealIP() ip: string) {
+        let size = body.image.length;
         this.imgurSrv.uploadToImgur(body.image).subscribe(r => {
             if(r.data.data) {
+                this.metricCollector.writeMetric('imgur-api', {
+                    size,
+                    ip
+                }, {
+                    status: 'ok'
+                });
                 response.send({image: r.data.data.link});
             } else {
+                this.metricCollector.writeMetric('imgur-api', { type: 'format-error', ip, size}, { status: 'err' });
                 response.status(400).send({error: 'IMGUR RESPONSE FORMAT ERROR.'});
             }
         }, e => {
+            this.metricCollector.writeMetric('imgur-api', { type: 'bad-gateway', ip, size }, { status: 'err' });
             response.status(502).send({error: 'IMGUR ERROR.'});
         })
     }
