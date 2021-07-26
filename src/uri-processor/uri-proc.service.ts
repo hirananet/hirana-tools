1eb69d0e5b45d2f7a1d7f596bd290e4d50b7da65
Alexander Eberle <alexander171294@gmail.com>
2021-07-26 16:19:13 -0300

    url processor rollbacked

--- src/uri-processor/uri-proc.service.ts
+++ src/uri-processor/uri-proc.service.ts
@@ -3,14 +3,13 @@ import { PublisherService } from './../utils/core-utils/publisher/publisher.serv
 import { SubscriberService } from './../utils/core-utils/subscriber/subscriber.service';
 import { environments } from 'src/environment';
 import { CacheRedisService } from './../utils/core-utils/cache-redis/cache-redis.service';
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable, Logger, HttpService } from '@nestjs/common';
 import { first } from 'rxjs/operators';
 
 const urlParser = require('url');
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
-const ogs = require('open-graph-scraper-lite');
 
 @Injectable()
 export class UriProcService {
@@ -20,7 +19,8 @@ export class UriProcService {
     constructor(private cacheSrv: CacheRedisService,
                 private subSrv: SubscriberService,
                 private pubSrv: PublisherService,
-                private metricCollector: MetricCollectorService) {
+                private metricCollector: MetricCollectorService,
+                private httpSrv: HttpService) {
         this.subSrv.attach('url-processed').subscribe((d) => {
 
         })
@@ -31,7 +31,6 @@ export class UriProcService {
         return new Promise<{title: string, favicon: string, status: string}>(async (res, rej) => {
             const urlChecksum = this.cacheSrv.checksum(url);
             const cache = await this.cacheSrv.getFromCache('url-'+urlChecksum, true);
-
             if(cache) {
                 if(cache.ready) {
                     this.metricCache('cached', urlChecksum, url);
@@ -65,38 +64,62 @@ export class UriProcService {
                 // send to cache
                 const cached = this.cacheSrv.saveInCache('url-'+urlChecksum, environments.urlTTL, data);
                 // get data
+                try {
+                    const _data = await this.parseResponse(url);
+                    data.title = _data.title;
+                    data.favicon = _data.favicon;
+                    data.ready = true;
+                    const updated = this.cacheSrv.saveInCache('url-'+urlChecksum, environments.urlTTL, data);
+                    if(updated) {
+                        this.pubSrv.publish('url-processed', {
+                            hash: urlChecksum,
+                            resolved: true
+                        });
+                    } else {
+                        this.logger.error('Fail update: ' + urlChecksum + '' + url);
+                        this.pubSrv.publish('url-processed', {
+                            hash: urlChecksum,
+                            resolved: false
+                        });
+                    }
+                    res(data);
+                } catch(e) {
+                    this.errorFetching(urlChecksum, url, cached);
+                    this.logger.error('Fail fetch: ', e);
+                    rej('fail fetch.');
+                }
                 
-                ogs({url: url})
-                    .then((data) => {
-                        const { error, result, response } = data;
-                        if(!error) {
-                            data.title = result.ogTitle;
-                            data.favicon = result.ogImage?.url;
-                            data.ready = true;
-                            const updated = this.cacheSrv.saveInCache('url-'+urlChecksum, environments.urlTTL, data);
-                            if(updated) {
-                                this.pubSrv.publish('url-processed', {
-                                    hash: urlChecksum,
-                                    resolved: true
-                                });
-                            } else {
-                                this.logger.error('Fail update: ' + urlChecksum + '' + url);
-                                this.pubSrv.publish('url-processed', {
-                                    hash: urlChecksum,
-                                    resolved: false
-                                });
-                            }
-                            res(data);
-                        } else {
-                            this.errorFetching(urlChecksum, url, cached);
-                            rej('fail fetch.');
-                        }
-                    }).catch(e => {
-                        this.errorFetching(urlChecksum, url, cached);
-                        this.logger.error('Fail fetch: ', e);
-                        rej('fail fetch.');
-                    })
             }
+        });
+    }
+
+    private parseResponse(url: string): Promise<{title:string, favicon: string}> {
+        return new Promise((res,rej) => {
+            this.httpSrv.get(url, {headers: {'User-Agent': environments.userAgent}, responseType: 'document', timeout: 2000})
+                        .subscribe(_res => {
+                            if(_res.data.length > 4096) {
+                                rej();
+                            }
+                            const dom = new JSDOM(_res.data);
+                            const out = {
+                                title: dom.window.document.title,
+                                favicon: undefined
+                            }
+                            const tq = dom.window.document.head.querySelector('link[rel=icon]');
+                            out.favicon =  tq ? tq.href : undefined;
+                            if(!out.favicon) {
+                                const tq2 = dom.window.document.head.querySelector('link[rel="shortcut icon"]');
+                                out.favicon = tq2 ? tq2.href : undefined;
+                            }
+                            const urlParsed = urlParser.parse(url);
+                            const urlBase = urlParsed.protocol + '//' + urlParsed.host;
+                            if (out.favicon && out.favicon.indexOf('http') != 0) {
+                                out.favicon = out.favicon[0] == '/' ? urlBase + out.favicon : urlBase + '/' + out.favicon;
+                            }
+                            res(out);
+                        }, err => {
+                            rej(err);
+                        });
         });
     }
     
